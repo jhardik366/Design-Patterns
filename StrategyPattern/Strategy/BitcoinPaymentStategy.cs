ee2c5676a371a9611167368386e96387de8461b2
jhardik366 <33342767+jhardik366@users.noreply.github.com>
2023-08-05 21:33:38 +0530

    added Strategy Pattern

---
+++ StrategyPattern/Strategy/BitcoinPaymentStategy.cs
@@ -0,0 +1,10 @@
+﻿namespace StrategyPattern.Strategy
+{
+    public class BitcoinPaymentStategy : IPaymentStrategy
+    {
+        public void ProcessPayment(int amount)
+        {
+            Console.WriteLine($"Processing Bitcoin payment for amount: {amount}");
+        }
+    }
+}
---
+++ StrategyPattern/Strategy/Interface/IPaymentStrategy.cs
@@ -0,0 +1,7 @@
+﻿namespace StrategyPattern.Strategy
+{
+    public interface IPaymentStrategy
+    {
+        public void ProcessPayment(int amount);
+    }
+}
