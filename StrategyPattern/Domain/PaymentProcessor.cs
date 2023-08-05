ae95013f47997ec965c695bf55ec98f1840989da
jhardik366 <33342767+jhardik366@users.noreply.github.com>
2023-08-05 21:34:55 +0530

    Add files via upload

---
+++ StrategyPattern/Domain/PaymentProcessor.cs
@@ -0,0 +1,25 @@
+﻿using StrategyPattern.Strategy;
+
+namespace StrategyPattern.Domain
+{
+    public class PaymentProcessor
+    {
+        IPaymentStrategy _paymentStrategy;
+
+        // constructor injection
+        public PaymentProcessor(IPaymentStrategy paymentStrategy)
+        {
+            _paymentStrategy = paymentStrategy;
+        }
+
+        public void SetPaymentStrategy(IPaymentStrategy paymentStrategy)
+        {
+            _paymentStrategy = paymentStrategy;
+        }
+
+        public void ProcessPayment(int amount)
+        {
+            _paymentStrategy.ProcessPayment(amount);
+        }
+    }
+}
